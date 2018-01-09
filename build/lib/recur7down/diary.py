612ea3e2738e55ed5161641adcc7234014691a1a
Shichao Ji <jshichao@vt.edu>
2018-01-09 00:37:26 -0500

    debug diary

--- build/lib/recur7down/diary.py
+++ build/lib/recur7down/diary.py
@@ -33,25 +33,34 @@ from multiprocessing.dummy import Pool as ThreadPool
 
 
 def One(ID):
-    link = diary.format(ID)
-    
-    base = json.loads(requests.get(link, timeout=6).text)
-    lists = base['group_list']['list']
-    df = pd.DataFrame(map(lambda p: flatten_dict(p, layers=3), lists))
-    
     
     try:
+        link = diary.format(ID)
+        r = requests.get(link, timeout=6)
+        base = json.loads(r.text)
+        lists = base['group_list']['list']
+        
+        df = pd.DataFrame(map(lambda p: flatten_dict(p, layers=3), lists))
+
         col = list(df.columns)
-        col = [col.pop(col.index('group_id')), col.pop(col.index('uid')), col.pop(col.index('user_name')), col.pop(col.index('top.summary'))]+col
+        col = [col.pop(col.index('group_id')), col.pop(col.index('uid')), col.pop(col.index('user_name')), col.pop(col.index('top.summary'))]+co
+l
         df = df[col]
         df.pop('other.summary')
         df['from_product']=ID
         df = df.set_index('from_product')
         df.to_excel(folder + str(ID)+'.xlsx', encoding='utf-8')
         print 'O',
+        
     except Exception as e:
+        print ID, df.shape,
+        print r.status_code,
         print e,
-        One(ID)
+        #if r.status_code==200:
+        if df.shape[0]==0:
+                pass
+        else:
+                One(ID)
 
 def diary_main():
     global diary, folder
@@ -80,11 +89,16 @@ def diary_main():
     content = pd.read_csv('./diary_products.csv', names=['product_id'])['product_id'].tolist()
     cpu = int(raw_input('(multi-processing) how many process to run ? '))
 
+    have = list(map(lambda x: x.split('.')[0] ,os.listdir(folder)))
+    want = list(set(content)-set(have))
+    
+    
+    
     start=time()
 
     pool = ThreadPool(cpu)
     
-    results = pool.map(One, content)
+    results = pool.map(One, want)
     
     pool.close()
     pool.join()
