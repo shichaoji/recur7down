7d2695c196de9f75373fb35b17625fca2a1382c5
Shichao Ji <jshichao@vt.edu>
2018-01-22 00:45:35 -0500

    "add"

--- recur7down/__init__.py
+++ recur7down/__init__.py
@@ -3,11 +3,12 @@ from .products import product_main
 from .diary import diary_main
 from .transaction import transaction_main
 from .html import html_main
+from .parse_diary import parse_diary_main
 
 
 
 def main():
-    c = raw_input('get product(p) or diary(d) or transaction(t) or html(h):  p/d/t/h? ')
+    c = raw_input('get product(p) or diary(d) or transaction(t) or html(h) or parse diary(pd):  \np/d/t/h.pd? ')
     if c.lower()=='p':
         product_main()
     elif c.lower()=='d':
@@ -16,3 +17,5 @@ def main():
         transaction_main()
     elif c.lower()=='h':
         html_main()
+    elif c.lower()=='pd':
+        parse_diary_main()
